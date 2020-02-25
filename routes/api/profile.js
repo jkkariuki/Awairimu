a5b6d5a91a420e1401053c0f27c93ac0ed6b2661
jk <jkkariuki15@gmail.com>
2020-02-25 13:00:01 -0500

    nodemailer updates

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -137,58 +137,56 @@ router.post(
 
       let adminUser = await AdminUser.findOne({ email: "akariuki@mail.com" });
 
-      //nodemailer
-      // const transport = {
-      //   service: "gmail",
-      //   host: "smtp.gmail.com",
-      //   auth: {
-      //     user: process.env.USER,
-      //     pass: process.env.PASS
-      //   }
-      // };
-
-      const auth = {
+      // nodemailer
+      const transport = {
+        service: "gmail",
+        host: "smtp.gmail.com",
         auth: {
-          api_key: process.env.API_KEY,
-          domain: process.env.DOMAIN
+          user: process.env.USER,
+          pass: process.env.PASS
         }
       };
 
-      const transporter = nodemailer.createTransport(mailGun(auth));
-
-      // transporter.verify((error, success) => {
-      //   if (error) {
-      //     console.log(error);
-      //   } else {
-      //     console.log("Server is ready to take messages");
+      // const auth = {
+      //   auth: {
+      //     api_key: process.env.API_KEY,
+      //     domain: process.env.DOMAIN
       //   }
-      // });
+      // };
+
+      var transporter = nodemailer.createTransport(transport);
+
+      transporter.verify((error, success) => {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Server is ready to take messages");
+        }
+      });
 
       let name = user.firstName + " " + user.lastName;
       let email = user.email;
       let message = JSON.stringify(req.body.formData);
       let content = `name: ${name} \n email: ${email} \n message: ${message} `;
 
-      const sendMail = (email, message) => {
-        const mailOptions = {
-          from: email,
-          to: "ajkariuki589@gmail.com", //Change to email address that you want to receive messages on
-          subject: "New Message from Contact Form",
-          text: message
-        };
-
-        transporter.sendMail(mailOptions, (err, data) => {
-          if (err) {
-            res.json({
-              msg: "fail"
-            });
-          } else {
-            res.json({
-              msg: "success"
-            });
-          }
-        });
+      const mailOptions = {
+        from: email,
+        to: "ajkariuki589@gmail.com", //Change to email address that you want to receive messages on
+        subject: "New Message from Contact Form",
+        text: message
       };
+
+      transporter.sendMail(mailOptions, (err, data) => {
+        if (err) {
+          res.json({
+            msg: "fail"
+          });
+        } else {
+          res.json({
+            msg: "success"
+          });
+        }
+      });
 
       // const newMessage = {
       //   userId: user._id,
