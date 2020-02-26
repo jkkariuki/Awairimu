9fc8ea3ae11de7370017bc71cb70f05204bb7a85
jk <jkkariuki15@gmail.com>
2020-02-26 17:52:49 -0500

    nodemailer configured

--- mail.js
+++ mail.js
@@ -1,11 +1,11 @@
-const config = require("config");
+// const config = require("config");
 const nodemailer = require("nodemailer");
 const mailGun = require("nodemailer-mailgun-transport");
 
 const auth = {
   auth: {
-    api_key: config.get("REACT_APP_API_KEY"),
-    domain: config.get("REACT_APP_DOMAIN")
+    api_key: JSON.stringify(process.env.REACT_APP_API_KEY),
+    domain: JSON.stringify(process.env.REACT_APP_DOMAIN)
   }
 };
 
