50f84ee6b179a0dbafe0ab5d8613138f4ef6a214
jk <jkkariuki15@gmail.com>
2020-02-26 03:04:02 -0500

    nodemailer configured

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+require("dotenv").config({ path: path.resolve(__dirname, "../.env") });
 const express = require("express");
 const connectDB = require("./config/db");
 const path = require("path");
