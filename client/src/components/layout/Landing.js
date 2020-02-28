0de0aacd4949a503bf766b53279034f49fc491e2
jk <jkkariuki15@gmail.com>
2020-02-28 02:14:49 -0500

    cleaned up code &  new animations

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -8,7 +8,16 @@ import "./Landing.css";
 const Landing = () => {
   return (
     <div>
-      <div className='jumbotron jumbotron-fluid landing-jumbo'></div>
+      <div className='jumbotron jumbotron-fluid landing-jumbo'>
+        <div class='container'>
+          <h1
+            id='landing-jumbo'
+            class='animated slideInLeft delay-2s display-4'
+          >
+            Let Your Search Begin
+          </h1>
+        </div>
+      </div>
 
       <SearchForm />
       <FeaturedListings />
